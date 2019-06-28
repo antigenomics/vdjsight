f99e669c6aa27e685adc7887bb93e67273650e3f
Dmitri Bagaev <bvdmitri@gmail.com>
2019-06-28 15:45:05 +0300

    fix(frontend): Zone UNPATCHED_EVENTS fix

--- frontend/src/zone-flags.ts
+++ frontend/src/zone-flags.ts
@@ -1,5 +1,5 @@
 // tslint:disable:no-any
 (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
 (window as any).__zone_symbol__UNPATCHED_EVENTS      = [
-  'scroll', 'mouseenter', 'mouseleave', 'mousemove', 'mouseover', 'mouseout', 'mousewheel', 'wheel'
+  'scroll', 'mousemove', 'mouseover', 'mouseout', 'mousewheel', 'wheel'
 ]; // disable patch specified eventNames
