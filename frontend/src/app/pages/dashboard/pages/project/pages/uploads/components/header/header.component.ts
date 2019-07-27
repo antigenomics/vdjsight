156973f010714de337acbe2db6d762ad6b0de7a9
Dmitri Bagaev <bvdmitri@gmail.com>
2019-07-27 22:03:12 +0300

    fix(frontend): Upload page fixes

---
+++ frontend/src/app/pages/dashboard/pages/project/pages/uploads/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector:        'vs-upload-header',
+  templateUrl:     './header.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {}
