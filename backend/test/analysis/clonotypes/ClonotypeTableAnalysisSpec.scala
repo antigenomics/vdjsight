84cf6fdaca623f3897879c3685d27138df047972
Dmitri Bagaev <bvdmitri@gmail.com>
2019-08-23 15:44:45 +0300

    fix(backend): Test fix

--- backend/test/analysis/clonotypes/ClonotypeTableAnalysisSpec.scala
+++ backend/test/analysis/clonotypes/ClonotypeTableAnalysisSpec.scala
@@ -3,13 +3,14 @@ package analysis.clonotypes
 import java.io.{BufferedInputStream, BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream}
 
 import analysis.AnalysisTestTag
+import com.antigenomics.mir.clonotype.io.ClonotypeTablePipe
 import com.antigenomics.mir.clonotype.parser.{ClonotypeTableParserUtils, Software}
-import com.antigenomics.mir.clonotype.table.ClonotypeTable
 import com.antigenomics.mir.clonotype.{Clonotype, ClonotypeCall}
 import com.antigenomics.mir.segment.{Gene, MissingDiversitySegment, MissingJoiningSegment, MissingVariableSegment}
 import com.antigenomics.mir.{CommonUtils, Species}
 import org.scalatest.{Assertion, Assertions}
 import specs.BaseTestSpec
+import utils.StreamUtils
 
 class ClonotypeTableAnalysisSpec extends BaseTestSpec {
 
@@ -32,48 +33,53 @@ class ClonotypeTableAnalysisSpec extends BaseTestSpec {
         r.j shouldEqual (if (bj != MissingJoiningSegment.INSTANCE) bj.getId else "")
       }
 
-      val clonotypesStream = ClonotypeTableParserUtils.streamFrom(
-        CommonUtils.getFileAsStream(getClass.getResource("/vdjtools.txt").getPath, false),
-        Software.VDJtools,
-        Species.Human,
-        Gene.TRB
-      )
+      val clonotypesStream = ClonotypeTableParserUtils
+        .streamFrom(
+          CommonUtils.getFileAsStream(getClass.getResource("/vdjtools.txt").getPath, false),
+          Software.VDJtools,
+          Species.Human,
+          Gene.TRB
+        )
+        .asInstanceOf[ClonotypeTablePipe[Clonotype]]
 
-      val table = new ClonotypeTable(clonotypesStream)
+      val stream = StreamUtils.makeLazyList(new StreamUtils.StreamLike[ClonotypeCall[Clonotype]] {
+        override def hasNext: Boolean               = clonotypesStream.hasNext
+        override def next: ClonotypeCall[Clonotype] = clonotypesStream.next()
+      })
 
       val bytes  = new ByteArrayOutputStream(10485760)
       val output = new BufferedOutputStream(bytes, 4096)
 
-      CachedClonotypeTable.write(output, table.getClonotypes)
+      val size = CachedClonotypeTable.write(output, stream)
 
       val input1  = new BufferedInputStream(new ByteArrayInputStream(bytes.toByteArray), 4096)
-      val cached1 = CachedClonotypeTable.read(input1)
+      val cached1 = CachedClonotypeTable.read(size, input1)
       var index1  = 0
       cached1
         .rows()
         .foreach(r => {
-          checkEquality(index1, table.getClonotypes.get(index1), r)
+          checkEquality(index1, stream.toSeq(index1), r)
           index1 += 1
         })
 
       val input2  = new BufferedInputStream(new ByteArrayInputStream(bytes.toByteArray), 16)
-      val cached2 = CachedClonotypeTable.read(input2)
+      val cached2 = CachedClonotypeTable.read(size, input2)
       var index2  = 100
       cached2
         .skip(100)
         .foreach(r => {
-          checkEquality(index2, table.getClonotypes.get(index2), r)
+          checkEquality(index2, stream.toSeq(index2), r)
           index2 += 1
         })
 
       val input3  = new BufferedInputStream(new ByteArrayInputStream(bytes.toByteArray), 65536)
-      val cached3 = CachedClonotypeTable.read(input3)
+      val cached3 = CachedClonotypeTable.read(size, input3)
       var index3  = 900
       cached3
         .skip(900)
         .take(10000)
         .foreach(r => {
-          checkEquality(index3, table.getClonotypes.get(index3), r)
+          checkEquality(index3, stream.toSeq(index3), r)
           index3 += 1
         })
 
